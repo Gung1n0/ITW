501dd6aea34daddc7c1e6b577d92a8f21b37cc76
Gung1n0 <gungino88@gmail.com>
2026-04-29 23:00:39 +0200

    modified:   Readme 	modified:   fade.js 	modified:   index.html 	modified:   style.css

--- project_2/fade.js
+++ project_2/fade.js
@@ -34,6 +34,7 @@ function fade_bottom(id, rate) {
 fade_top("#home", 2);
 fade_top("#profile", 2);
 fade_top("#school", 1.5);
-fade_top("#projects", 0.1);
+fade_top("#sch-p", 0.5);
+fade_top("#pers-p", 0.5);
 fade_bottom("#skills", 8);
 fade_bottom("#kontakt", 1);
